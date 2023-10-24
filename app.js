55d0fd0087f7138d71ecff825faf88c6365d0307
Jerry Andrianto Pangaribuan <jerryandrianto22@gmail.com>
2023-10-24 12:25:20 +0700

    add apiRouter

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/db_gigih_capstone');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var apiRouter = require('./routes/api');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
